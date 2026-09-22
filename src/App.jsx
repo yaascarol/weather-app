edaf8088fcee8af03efd47d24ac86e2625f4d51c
YASMIN CAROLINA FERNANDES LOPES <yasmin.cflopes@senacsp.edu.br>
2026-09-22 19:25:19 -0300

    pushing changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WheatherApp from './assets/components/WeatherApp'
+import './assets/components/WeatherApp.css'
+
+const App = () => {
+  return (
+    <div>
+      <WheatherApp />
+    </div>
+  )
+}
+
+export default App
